6abc6efa08e5f11ce81c5e5665652e3a2d717b87
Belal Safy <belal.aglan2@gmail.com>
2023-09-16 15:24:34 +0300

    first

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+# from keras.models import load_model
+from PIL import Image
+
+from util import classify
+
+# set title
+st.title('Pneumonia classification')
+
+# set header
+st.header('Please upload a chest X-ray image')
+
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# load classifier
+# model = load_model('./model/pneumonia_classifier.h5')
+model = "sdf"
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    class_name = classify(image, model)
+
+    # write classification
+    st.write("## {}".format(class_name))
